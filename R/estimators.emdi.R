1c22a62a827bb0c0e4e609067427732e278dea17
Kreutzmann <akreutzmann@fu-berlin.de>
2016-11-02 10:51:26 +0100

    update reaktion auf submission

--- R/estimators.emdi.R
+++ R/estimators.emdi.R
@@ -57,7 +57,7 @@ estimators <- function(object, ...) UseMethod("estimators")
 #' self_empl + unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + 
 #' fam_allow + house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop,
 #' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district",
-#' pov_line = 10722.66, transformation = "box.cox", L= 2, MSE = TRUE, B = 2,
+#' pov_line = 10722.66, transformation = "box.cox", L= 1, MSE = TRUE, B = 1,
 #' custom_indicator = list( my_max = function(y, pov_line){max(y)},
 #' my_min = function(y, pov_line){min(y)}), na.rm = TRUE, cpus = 1)
 #'
