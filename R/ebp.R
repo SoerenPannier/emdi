ae446846102196c3e130a6160775a5d58aeac38e
Kreutzmann <akreutzmann@fu-berlin.de>
2017-01-09 13:21:39 +0100

    space deletion

--- R/ebp.R
+++ R/ebp.R
@@ -88,7 +88,7 @@
 #' data("eusilcA_smp")
 #'
 #' # Example with default setting but na.rm=TRUE
-#' set.seed(100); emdi_model <- ebp( fixed = eqIncome ~ gender + eqsize + cash + 
+#' set.seed(100); emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + 
 #' self_empl + unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + 
 #' fam_allow + house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop,
 #' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district",
@@ -96,7 +96,7 @@
 #' 
 #' 
 #' # Example with two additional indicators
-#' set.seed(100); emdi_model <- ebp( fixed = eqIncome ~ gender + eqsize + cash + 
+#' set.seed(100); emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + 
 #' self_empl + unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + 
 #' fam_allow + house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop,
 #' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district",
