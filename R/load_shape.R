defd428719b411baa9207779c83eca5017440caa
Kreutzmann <akreutzmann@fu-berlin.de>
2017-01-11 11:20:56 +0100

    load shape austria

---
+++ R/load_shape.R
@@ -0,0 +1,12 @@
+#' Loading the shape file for austrian district
+#'
+#' @return A shape file of class \code{SpatialPolygonsDataFrame}.
+#' @details The shape file contains the borders of Austrian districts. Thus, it
+#' can be used for the visualiuzation of estimation results for Austrian
+#' districts.
+#' @export
+
+load_shapeaustria <- function(){
+  load(system.file("shapes/shape_austria_dis.RData", package="emdi"))
+  .GlobalEnv$shape_austria_dis <- shape_austria_dis    
+}
