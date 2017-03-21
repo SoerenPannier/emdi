27f29633c66f5e4686dc76b5a8e3c6f628378ea0
Kreutzmann <akreutzmann@fu-berlin.de>
2017-03-21 15:38:05 +0100

    function description added to documentation of emdi

--- R/emdi.R
+++ R/emdi.R
@@ -2,15 +2,22 @@
 #'
 #' The package \code{emdi} supports estimating and mapping regional
 #' disaggregated indicators. For estimating these indicators direct estimation 
-#' and the Empirical Best Prediction approach by \cite{Molina and Rao (2010)} is 
-#' provided. Estimates of the mean square error for these methods can be 
+#' and the Empirical Best Prediction approach by \cite{Molina and Rao (2010)} 
+#' are provided. Estimates of the mean squared error for these methods can be 
 #' conducted by using a parametric bootstrap approach (\cite{Gonzalez-Manteiga et al. 2008}). 
 #' Furthermore, a mapping tool for plotting the estimates on their geographic 
 #' regions is provided. Point and uncertainty measures as well as diagnostic 
 #' tests can be easily extracted to excel.
 #'
 #' @details
-#' An overview of currently provided functions can be requested by
+#' The two estimation functions are called \code{\link{direct}} and 
+#' \code{\link{ebp}}. For both functions several methods are available as 
+#' \code{\link{estimators.emdi}}, \code{\link{plot.emdi}} (only for emdi objects 
+#' obtained by function \code{ebp}), \code{\link{print.emdi}} and 
+#' \code{\link{summary.emdi}}. Furthermore, functions \code{\link{map_plot}} and
+#' \code{\link{write.excel}} help to visualize and export results.
+#' 
+#' An overview of all currently provided functions can be requested by
 #' \code{library(help=emdi)}.
 #'
 #' @references
