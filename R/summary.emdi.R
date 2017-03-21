696bb3fc546c270bbf9218d8fb5d3a17e53b07bd
Kreutzmann <akreutzmann@fu-berlin.de>
2017-03-21 15:43:48 +0100

    return only one data set and use original sample data to add data to the model lme object for calculation of R2

--- R/summary.emdi.R
+++ R/summary.emdi.R
@@ -146,6 +146,7 @@ summary.emdi <- function(object, ...) {
                        row.names = c("Error", "Random_effect")
     )
     
+    #object$model$data <- object$framework$smp_data
     r_squared <- r.squaredGLMM(object$model)
     r_marginal <- r_squared[1]
     r_conditional <- r_squared[2]
