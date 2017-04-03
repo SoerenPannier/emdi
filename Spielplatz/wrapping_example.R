8d0e2c1a3c9f763d34396ead6ebf2c44587a5d63
Kreutzmann <akreutzmann@fu-berlin.de>
2017-04-03 17:43:50 +0200

    new function for wrapper

--- Spielplatz/wrapping_example.R
+++ Spielplatz/wrapping_example.R
@@ -34,3 +34,38 @@ emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl +
 estimators(emdi_model, indicator = "custom")
 
 rmpg(eusilcA_smp$eqIncome, breakdown = eusilcA_smp$district)
+
+
+# New example
+install.packages("ineq")
+library("ineq")
+
+
+
+set.seed(100)
+emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl + 
+                    unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + 
+                    rent + fam_allow + house_allow + cap_inv + tax_adj,
+                  pop_data = eusilcA_pop,
+                  pop_domains = "district",
+                  smp_data = eusilcA_smp,
+                  smp_domains = "district",
+                  na.rm = TRUE,
+                  L = 5, 
+                  B = 5, 
+                  MSE = T, 
+                  threshold = 1500,
+                  custom_indicator = list(theil = function(y, threshold){ineq(x = y, type = "Theil")})
+)
+
+
+emdi_direct <- direct(y="eqIncome", smp_data=eusilcA_smp, smp_domains="district", weights = "weight",     
+                      var=TRUE, B=50, seed=123, 
+                      custom_indicator = list(theil = function(y, weights, threshold){ineq(x = y, type = "Theil")}), na.rm=TRUE)
+
+
+theil_districts <- tapply(eusilcA_smp$eqIncome, droplevels(eusilcA_smp$district), ineq, type="Theil")
+
+all.equal(as.numeric(theil_districts),
+          estimators(emdi_direct, indicator = "theil")$ind$theil)
+
