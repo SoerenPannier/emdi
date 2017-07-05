888d31345c3b0de2ad99099eb5c80f7f34360bc9
Pannier <spannier@fu-berlin.de>
2017-07-05 11:20:47 +0200

    improved argument checking

--- R/check_ebp_arguments.R
+++ R/check_ebp_arguments.R
@@ -114,18 +114,44 @@ ebp_check2 <- function(threshold, transformation, interval, MSE, boot_type, B,
 # Functions called in notation
 fw_check1 <- function(pop_data, mod_vars, pop_domains, smp_data, 
                       fixed, smp_domains) {
-  if (!(all(mod_vars %in% colnames(pop_data)) && (pop_domains %in% colnames(pop_data)))) {
-    stop('Both the variable name in pop_domains and the explanatory variables
-         in argument fixed need to be contained in pop_data. Check variable 
-         names.')
+  #if (!(all(mod_vars %in% colnames(pop_data)) && (pop_domains %in% colnames(pop_data)))) {
+  #  stop('Both the variable name in pop_domains and the explanatory variables
+  #       in argument fixed need to be contained in pop_data. Check variable 
+  #       names.')
+  #}
+  if (!all(mod_vars %in% colnames(pop_data))) {
+    stop(paste0("Variable ", mod_vars[which(!(mod_vars %in% colnames(smp_data)))], " is not contained in pop_data.
+                Please provide valid variable names for the explanatory variables."))
   }
-  if (!(all(mod_vars %in% colnames(smp_data)) && (smp_domains %in% colnames(smp_data))
-        && (as.character(fixed[2])) %in% colnames(smp_data))) {
-    stop('The variable name in smp_domains and the variables
-         in argument fixed need to be contained in smp_data. Check variable
-         names.')
+  if (!(pop_domains %in% colnames(pop_data))) {
+    stop(paste0("The domain variable ", pop_domains, " is not contained in pop_data.
+                Please provide valid variable name for pop_domains."))
+  }
+ # if (!(all(mod_vars %in% colnames(smp_data)) && (smp_domains %in% colnames(smp_data))
+#        && (as.character(fixed[2])) %in% colnames(smp_data))) {
+#    stop('The variable name in smp_domains and the variables
+#         in argument fixed need to be contained in smp_data. Check variable
+#         names.')
+#  }
+  if (!all(mod_vars %in% colnames(smp_data))) {
+    stop(paste0("Variable ", mod_vars[which(!(mod_vars %in% colnames(smp_data)))], " is not contained in smp_data.
+                 Please provide valid variable names for the explanatory variables."))
+  }
+  if (!(smp_domains %in% colnames(smp_data))) {
+    stop(paste0("The domain variable ", smp_domains, " is not contained in smp_data.
+                 Please provide valid variable name for smp_domains."))
+  }
+  if (!((as.character(fixed[2])) %in% colnames(smp_data))) {
+    stop(paste0("Variable ", as.character(fixed[2]), " is not contained in smp_data. 
+                Please provide valid variable name for the dependent variable."))
+  }
+  
+ if (!is.numeric(smp_data[[paste(fixed[2])]])) {
+   stop(paste0(as.character(fixed[2])," must be the name of a variable that 
+               is a numeric vector."))
   }
 }
+
 
 
 
