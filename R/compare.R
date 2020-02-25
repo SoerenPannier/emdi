d46d076f60ce6da2063f9c6bef2e9e2cd9981e01
sharmening <sylvia.selmaier@gmx.de>
2020-02-25 14:04:21 +0100

    one function "compare" instead of "brown" and "syndir"

---
+++ R/compare.R
@@ -0,0 +1,97 @@
+#' Compare function
+#'
+#' This function assesses the quality of the model-based estimates by comparing 
+#' them with the direct estimates based on a goodness-of-fit test proposed by
+#' Brown et al. (2001) and by computing the correlation between the 
+#' regression-synthetic part of the Fay-Herriot model and the direct estimates.
+#'
+#' @param object an object of type "fh".
+#' @param ... further arguments passed to or from other methods.
+#' @return The null hypothesis, the value W of the test statistic, the degrees 
+#' of freedom and the p value of the Brown test and the correlation coefficient 
+#' of the synthetic part and the direct estimator.
+#' @references 
+#' Brown, G., R. Chambers, P. Heady, and D. Heasman (2001). Evaluation of small 
+#' area estimation methods: An application to unemployment estimates from the UK
+#' LFS. Symposium 2001 - Achieving Data Quality in a Statistical Agency: A 
+#' Methodological Perspective, Statistics Canada.
+#' @export
+
+compare <- function(object, ...) UseMethod("compare")
+#' Compare function
+#'
+#' This function assesses the quality of the model-based estimates by comparing 
+#' them with the direct estimates based on a goodness-of-fit test proposed by
+#' Brown et al. (2001) and by computing the correlation between the 
+#' regression-synthetic part of the Fay-Herriot model and the direct estimates.
+#'
+#' @param object an object of type "fh".
+#' @param ... further arguments passed to or from other methods.
+#' @return The null hypothesis, the value W of the test statistic, the degrees 
+#' of freedom and the p value of the Brown test and the correlation coefficient 
+#' of the synthetic part and the direct estimator.
+#' @references 
+#' Brown, G., R. Chambers, P. Heady, and D. Heasman (2001). Evaluation of small 
+#' area estimation methods: An application to unemployment estimates from the UK
+#' LFS. Symposium 2001 - Achieving Data Quality in a Statistical Agency: A 
+#' Methodological Perspective, Statistics Canada.
+#' @export
+
+compare.fh <- function(object, ...){
+
+  W_BL <- sum((object$ind$Direct[object$ind$Out == 0] - 
+                 object$ind$FH[object$ind$Out == 0])^2 /
+              (object$MSE$Direct[object$MSE$Out == 0] + 
+                 object$MSE$FH[object$MSE$Out == 0]))
+  
+ # Degress of freedom
+ df_BL <- object$framework$N_dom_smp
+ 
+ # p Value
+ p_value_BL <- 1 - pchisq(W_BL, df_BL)
+ 
+ testresults <- data.frame(W.value = W_BL,
+                           Df = df_BL,
+                           p.value = p_value_BL)
+ 
+ # Extraction of the regression part
+ xb <- (object$ind$FH[object$ind$Out == 0] - 
+          object$model$gamma$Gamma *
+          object$ind$Direct[object$ind$Out == 0]) /
+   (1 - object$model$gamma$Gamma)
+ # Direkt estimator
+ direct_insample <- object$ind$Direct[object$ind$Out == 0]
+ # Correlation
+ syndircor <- cor(xb, direct_insample)
+ 
+ results <- list(Brown = testresults, syndir = syndircor)
+ class(results) <- "compare.fh"
+ return(results)
+}
+
+#' Prints compare.fh objects
+#'
+#' @param x an object of type "compare.fh".
+#' @param ... further arguments passed to or from other methods.
+#' @export
+
+print.compare.fh <- function(x, ...)
+{
+  cat("\n")
+  cat("         Brown test ","\n")
+  cat("\n")
+  cat("Null hypothesis: EBLUP estimates do not differ significantly from the 
+      direct estimates","\n")
+  cat("\n")
+  print(data.frame(W.value = x[[1]]$W,
+                   Df = x[[1]]$Df,
+                   p.value = x[[1]]$p.value,
+                   row.names = ""))
+  cat("\n")
+  cat("Correlation between synthetic part and direct estimator: ", 
+      round(x[[2]],2),"\n")
+}
+
+
+
+
