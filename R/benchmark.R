7927e75fd7f02ae08b3756f7e68b8c378ee1db80
sharmening <sylvia.selmaier@gmx.de>
2020-03-18 12:15:07 +0100

    add overwrite argument and examples

--- R/benchmark.R
+++ R/benchmark.R
@@ -11,28 +11,49 @@
 #' (i) Raking ("\code{raking}"),
 #' (ii) Ratio adjustment ("\code{ratio}"),
 #' (iii) MSE adjustment ("\code{MSE_adj}")
-#' @return a list with following components: 
-#' \tabular{lll}{
-#'      \tab \code{benchmark_data} \tab data frame containing a domain indicator 
-#'      (Domain), direct estimates (Direct),  \cr
-#'    \tab \tab point predictions (FH), benchmarked point predictions (FHBENCH) and 
-#' a  \cr
-#' \tab \tab variable indicating out-of-sample domains Out (1 for out-of-sample, 0 for 
-#' in- \cr
-#' \tab \tab sample) .\cr
-#'      \tab \code{benchmark_type} \tab character returning the type of the 
-#'      benchmark method. \cr
-#' }
+#' @param overwrite if \code{TRUE}, the EBLUP results of the fh object are 
+#' substituted by the respective benchmarked results. Defaults
+#' to \code{FALSE}. 
+#' @return a data frame containing a domain indicator (Domain), direct estimates 
+#' (Direct), point predictions (FH), benchmarked point predictions (FHBENCH) and 
+#' a variable indicating out-of-sample domains Out (1 for out-of-sample, 0 for 
+#' in-sample) . If overwrite is set to TRUE, the fh object is returned, but the 
+#' point predictions of the ind data frame are substituted by the benchmarked 
+#' results.
 #' @references 
 #' Datta,G. S., Ghosh, M., Steorts, R. and Maples, J. (2010) Bayesian 
 #' benchmarking with applications to small area estimation. Test, 20, 574–588.
+#' @examples 
+#' # Loading data - population and sample data
+#' data("eusilcA_popAgg")
+#' data("eusilcA_smpAgg")
+#' 
+#' # Combine sample and population data -------------------------------------------
+#' combined_data <- combine_data(pop_data = eusilcA_popAgg, pop_domains = "Domain",
+#'                              smp_data = eusilcA_smpAgg, smp_domains = "Domain")
+#'
+#' # Estimate Fay-Herriot model
+#' fh_std <- fh(fixed = Mean ~ cash + self_empl, vardir = "Var_Mean",
+#' combined_data = combined_data, domains = "Domain", method = "ml", 
+#' interval = c(0, 100000000), MSE = TRUE)  
+#' 
+#' # Benchmark the point estimates
+#' 
+#' # Example 1: receive data frame with point estimates and their benchmarked results
+#' fh_bench <- benchmark(fh_std, benchmark = 20140.09, 
+#' eta = eusilcA_popAgg$ratio_n, type = "ratio")
+#' 
+#' # Example 2: overwrite the point estimates of the fh object with their benchmarked 
+#' results
+#' fh_bench <- benchmark(fh_std, benchmark = 20140.09, 
+#' eta = eusilcA_popAgg$ratio_n, type = "ratio", overwrite = TRUE)
 #' @export
 
 
-benchmark <- function(object, benchmark, eta, type) {
+benchmark <- function(object, benchmark, eta, type, overwrite = FALSE) {
   
   check_benchmark_arguments(object = object, benchmark = benchmark, eta = eta, 
-                            type = type)
+                            type = type, overwrite = overwrite)
   
   estim <- object$ind$FH
   FH_bench <- rep(NA, length(object$ind$FH))
@@ -48,12 +69,18 @@ benchmark <- function(object, benchmark, eta, type) {
     FH_bench <- estim + (1/(sum(eta^2/phi))) * 
       (benchmark - sum(eta * estim)) * (eta / phi)
   }
-  EBLUP_data_bench <- data.frame(Domain = object$ind$Domain)
-  EBLUP_data_bench$Direct <- object$ind$Direct
-  EBLUP_data_bench$FH[object$ind$Out == 0] <- object$ind$FH
-  EBLUP_data_bench$FHBENCH[object$ind$Out == 0] <- FH_bench
-  EBLUP_data_bench$Out <- object$ind$Out
-  result <- list(benchmark_data = EBLUP_data_bench, benchmark_type = type)
+  if (overwrite == FALSE){
+    EBLUP_data_bench <- data.frame(Domain = object$ind$Domain)
+    EBLUP_data_bench$Direct <- object$ind$Direct
+    EBLUP_data_bench$FH[object$ind$Out == 0] <- object$ind$FH
+    EBLUP_data_bench$FHBENCH[object$ind$Out == 0] <- FH_bench
+    EBLUP_data_bench$Out <- object$ind$Out
+    result <- EBLUP_data_bench
+  }
+  else {
+    object$ind$FH <- FH_bench
+    result <- object
+  }
   return(result)
 }
 
@@ -61,7 +88,7 @@ benchmark <- function(object, benchmark, eta, type) {
 
 ################################################################################
 # Argument checking
-check_benchmark_arguments <- function(object, benchmark, eta, type){
+check_benchmark_arguments <- function(object, benchmark, eta, type, overwrite){
   if(!inherits(object, "fh")){
     stop('Object needs to be fh object.')
   }
@@ -77,6 +104,10 @@ check_benchmark_arguments <- function(object, benchmark, eta, type){
                                                  type == "MSE_adj")) {
     stop("Type must be a character. The three options for types are ''raking'', 
          ''ratio'' and ''MSE_adj''.")
+  }
+  if (!is.logical(overwrite) || length(overwrite) != 1) {
+    stop("overwrite must be a logical value. Set overwrite to TRUE or FALSE. The 
+          default is set to FALSE. See also help(benchmark).")
   }
 }
 
