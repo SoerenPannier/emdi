3d461e9c357c130157f767f8c0af69d61d34bf20
Ann-KristinKreutzmann <annkristinkreutzmann@gmail.com>
2020-11-11 14:06:22 -0500

    some methods ranef, residuals, coef, logLik

--- R/get_methods_emdi.R
+++ R/get_methods_emdi.R
@@ -154,6 +154,8 @@ getResponse.emdi <- function(object, ...) {
   response
 }
 
+
+
 #' Extract fixed effects from an emdi object
 #'
 #' Method \code{fixef.emdi} extracts the fixed effects from an emdi 
@@ -161,11 +163,10 @@ getResponse.emdi <- function(object, ...) {
 #' 
 #' @param object an object of type "emdi".
 #' @param ... additional arguments that are not used in this method.
-#' @return For class ebp a vector containing the fixed effects is returned. 
-#' For class fh a data frame containing the fixed effects and the corresponding 
-#' standard errors, t- and p-value is returned. For class direct no fixed effects 
-#' are available.
-#' @seealso \code{\link{ebp}}, \code{\link{fh}}, \code{\link[nlme]{fixef}}
+#' @return For class ebp and fh a vector containing the fixed effects is 
+#' returned. For class direct no fixed effects are available.
+#' @seealso \code{\link{ebp}}, \code{\link{fh}}
+#' @aliases fixed.effects
 #' @examples
 #' \donttest{
 #' # Example for class ebp
@@ -178,7 +179,8 @@ getResponse.emdi <- function(object, ...) {
 #' fixef(emdi_model)
 #' }
 #' @export
-#' @importFrom nlme fixef
+#' @method fixef emdi
+#' @importFrom nlme fixef fixed.effects
 
 fixef.emdi <- function(object, ...) {
   
@@ -191,8 +193,220 @@ fixef.emdi <- function(object, ...) {
   } else if(inherits(object, "direct")){
     cat("For an object of class direct no fixed effects are available.")
   } else if (inherits(object, "fh")) {
-    object$model$coefficients
+    fixed_effects <- object$model$coefficients$coefficients
+    names(fixed_effects) <- row.names(object$model$coefficients)
+    fixed_effects
   }
 }
+
+
+#' Extract residuals of emdi objects
+#'
+#' Method \code{residuals.emdi} extracts the residuals from an emdi 
+#' object.
+#' 
+#' @param object an object of type "emdi".
+#' @param ... additional arguments that are not used in this method.
+#' @return For class ebp a vector containing the residuals is returned. 
+#' For class fh a data frame containing the realized and standardized realized 
+#' residuals. For class direct no residuals are available.
+#' @seealso \code{\link{ebp}}, \code{\link{fh}}
+#' @aliases resid
+#' @examples
+#' \donttest{
+#' # Example for class ebp
+#' emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl + 
+#' unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + fam_allow + 
+#' house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop, 
+#' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district", 
+#' na.rm = TRUE)
+#' 
+#' residuals(emdi_model)
+#' }
+#' @export
+#' @method residuals emdi
+#' @importFrom stats residuals resid
+
+residuals.emdi <- function(object, ...) {
+  
+  if(!inherits(object, "emdi")){
+    stop('First object needs to be of class emdi.')
+  }
+  
+  if(inherits(object, "ebp")){
+    as.numeric(object$model$residuals[, 2])
+  } else if(inherits(object, "direct")){
+    cat("For an object of class direct no fixed effects are available.")
+  } else if (inherits(object, "fh")) {
+    data.frame(real_residuals = object$model$real_residuals, 
+               std_real_residuals = object$model$std_real_residuals)
+  }
+}
+
+#' Extract random effects of emdi objects
+#'
+#' Method \code{ranef.emdi} extracts the random effects from an emdi 
+#' object.
+#' 
+#' @param object an object of type "emdi".
+#' @param ... additional arguments that are not used in this method.
+#' @return For class ebp a vector containing the residuals is returned. 
+#' For class fh a data frame containing the realized and standardized realized 
+#' residuals. For class direct no residuals are available.
+#' @seealso \code{\link{ebp}}, \code{\link{fh}}
+#' @aliases random.effects
+#' @examples
+#' \donttest{
+#' # Example for class ebp
+#' emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl + 
+#' unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + fam_allow + 
+#' house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop, 
+#' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district", 
+#' na.rm = TRUE)
+#' 
+#' ranef(emdi_model)
+#' }
+#' @export
+#' @method ranef emdi
+#' @importFrom nlme ranef random.effects
+
+ranef.emdi <- function(object, ...) {
+  
+  if(!inherits(object, "emdi")){
+    stop('First object needs to be of class emdi.')
+  }
+  
+  if(inherits(object, "ebp")){
+    ranef(object$model)
+  } else if(inherits(object, "direct")){
+    cat("For an object of class direct no fixed effects are available.")
+  } else if (inherits(object, "fh")) {
+    object$model$random_effects
+  }
+}
+
+
+
+#' Extract model coefficients of emdi objects
+#'
+#' Method \code{coef.emdi} extracts the model coefficients from an emdi 
+#' object.
+#' 
+#' @param object an object of type "emdi".
+#' @param ... additional arguments that are not used in this method.
+#' @return For class ebp a vector containing the residuals is returned. 
+#' For class fh a data frame containing the realized and standardized realized 
+#' residuals. For class direct no residuals are available.
+#' @seealso \code{\link{ebp}}, \code{\link{fh}}
+#' @aliases coefficients
+#' @examples
+#' \donttest{
+#' # Example for class ebp
+#' emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl + 
+#' unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + fam_allow + 
+#' house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop, 
+#' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district", 
+#' na.rm = TRUE)
+#' 
+#' coef(emdi_model)
+#' }
+#' @export
+#' @method coef emdi
+#' @importFrom stats coef coefficients
+
+coef.emdi <- function(object, ...) {
+  
+  if(!inherits(object, "emdi")){
+    stop('First object needs to be of class emdi.')
+  }
+  
+  if(inherits(object, "ebp")){
+    coef(object$model)
+  } else if(inherits(object, "direct")){
+    cat("For an object of class direct no fixed effects are available.")
+  } else if (inherits(object, "fh")) {
+    fixed_effects <- object$model$coefficients$coefficients
+    names(fixed_effects) <- row.names(object$model$coefficients)
+    fixed_effects
+  }
+}
+
+
+#' Extract log-Likelihood of emdi objects
+#'
+#' Method \code{logLik.emdi} extracts the log-Likelihood from an emdi 
+#' object.
+#' 
+#' @param object an object of type "emdi".
+#' @param ... additional arguments that are not used in this method.
+#' @return For class ebp and fh, a vector containing the log-Likelihood. For 
+#' class direct, no residuals are available.
+#' @seealso \code{\link{ebp}}, \code{\link{fh}}
+#' @examples
+#' \donttest{
+#' # Example for class ebp
+#' emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl + 
+#' unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + fam_allow + 
+#' house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop, 
+#' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district", 
+#' na.rm = TRUE)
+#' 
+#' logLik(emdi_model)
+#' }
+#' @export
+#' @method logLik emdi
+#' @importFrom stats logLik
+
+logLik.emdi <- function(object, ...) {
+  
+  if(!inherits(object, "emdi")){
+    stop('First object needs to be of class emdi.')
+  }
+  
+  if(inherits(object, "ebp")){
+    object$model$logLik
+  } else if(inherits(object, "direct")){
+    cat("For an object of class direct no fixed effects are available.")
+  } else if (inherits(object, "fh")) {
+    object$model$model_select$loglike
+  }
+}
+
+
+#' Predictions from emdi objects
+#'
+#' Method \code{predict.emdi} extracts the small area predictions from an emdi 
+#' object.
+#' 
+#' @param object an object of type "emdi".
+#' @param ... additional arguments that are not used in this method.
+#' @return Data frame with domain predictors.
+#' @seealso \code{\link{direct}}, \code{\link{ebp}}, \code{\link{fh}}
+#' @examples
+#' \donttest{
+#' # Example for class ebp
+#' emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl + 
+#' unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + fam_allow + 
+#' house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop, 
+#' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district", 
+#' na.rm = TRUE)
+#' 
+#' predict(emdi_model)
+#' }
+#' @export
+#' @method predict emdi
+
+predict.emdi <- function(object, ...) {
+  
+  if(!inherits(object, "emdi")){
+    stop('First object needs to be of class emdi.')
+  }
+  
+  object$ind
+}
+
+
+
+
 
 
---
+++ R/qqnorm.R
@@ -0,0 +1,118 @@
+#' Quantile-quantile plots for an emdi object
+#'
+#' Normal quantile-quantile plots of the underlying model in the EBP (see also
+#' \code{\link{ebp}}) or Fay-Herriot (see also \code{\link{fh}}) approaches are 
+#' obtained. The plots are obtained by \code{\link[ggplot2]{ggplot}}.
+#' @param y an object of type "emdi", "model", representing point and, if chosen,
+#' MSE estimates obtained by the EBP or Fay-Herriot approach (see also
+#' \code{\link{ebp}} and \code{\link{fh}}).
+#' @param color a character vector with two elements. The first element defines
+#' the color for the line in the QQ-plots, for the Cook's Distance plot and for
+#' the Box-Cox plot. The second element defines the color for the densities.
+#' @param gg_theme \code{\link[ggplot2]{theme}} list from package \pkg{ggplot2}.
+#' For using this argument, package \pkg{ggplot2} must be loaded via
+#' \code{library(ggplot2)}. See also Example 4.
+#' @param ... optional arguments passed to generic function.
+#' @return Two Q-Q plots in one grid, two density plots, a Cook's distance plot
+#' and a likelihood plot for the optimal parameter of the Box-Cox transformation
+#' obtained by \code{\link[ggplot2]{ggplot}}. The latter two plots are only provided
+#' for ebp object.
+#' @seealso \code{\link{emdiObject}}, \code{\link{ebp}}, \code{\link{fh}}
+#' @examples
+#' \donttest{
+#' # Examples for models of type ebp
+#' # Loading data - population and sample data
+#' data("eusilcA_pop")
+#' data("eusilcA_smp")
+#'
+#' # With default setting but na.rm = TRUE; with Box-Cox transformation
+#' emdi_model <- ebp(fixed = eqIncome ~ gender + eqsize + cash + self_empl +
+#' unempl_ben + age_ben + surv_ben + sick_ben + dis_ben + rent + fam_allow +
+#' house_allow + cap_inv + tax_adj, pop_data = eusilcA_pop,
+#' pop_domains = "district", smp_data = eusilcA_smp, smp_domains = "district",
+#' na.rm = TRUE)
+#'
+#' # Example 1: Creation of default diagnostic plots
+#' qqnorm(emdi_model)
+#'
+#' # Example for models of type fh
+#' 
+#' # Loading data - population and sample data
+#' data("eusilcA_popAgg")
+#' data("eusilcA_smpAgg")
+#'
+#' # Combine sample and population data
+#' combined_data <- combine_data(pop_data = eusilcA_popAgg, pop_domains = "Domain",
+#'                               smp_data = eusilcA_smpAgg, smp_domains = "Domain")
+#'
+#' # Generation of the emdi object
+#' fh_std <- fh(fixed = Mean ~ cash + self_empl, vardir = "Var_Mean",
+#'              combined_data = combined_data, domains = "Domain", method = "ml", 
+#'              MSE = TRUE)
+#'
+#' # Example 5: Creation of default diagnostic plots for Fay-Herriot model
+#' qqnorm(fh_std)
+#'
+#' }
+#' @export 
+#' @method qqnorm emdi
+#' @importFrom ggplot2 qplot geom_abline ggtitle ylab xlab ggplot stat_qq
+#' @importFrom ggplot2 aes geom_point geom_smooth coord_fixed geom_line
+#' @importFrom ggplot2 scale_color_manual scale_fill_manual geom_segment
+#' @importFrom ggplot2 scale_linetype_discrete geom_density geom_text
+#' @importFrom ggplot2 geom_line geom_vline stat_function geom_qq
+#' @importFrom nlme ranef random.effects
+#' @importFrom gridExtra arrangeGrob grid.arrange
+#' @importFrom stats shapiro.test logLik cooks.distance
+#' @importFrom HLMdiag mdffits
+
+qqnorm.emdi <- function(y, color = c("blue", "lightblue3"),
+                        gg_theme = NULL, ...) {
+  
+  Residuals <- Random <- index <- lambda <- log_likelihood <- NULL
+  # avoid note due to ggplot2
+  # Preparation for plots
+  
+  if (inherits(y, "ebp")) {
+    residuals <- residuals(y$model, level = 0, type = "pearson")
+    rand.eff <- nlme::ranef(y$model)$'(Intercept)'
+    srand.eff <- (rand.eff - mean(rand.eff)) / sd(rand.eff)
+    tmp <- as.matrix(random.effects(y$model))[,1]
+    
+    model <- y$model
+    model$call$fixed <- y$fixed
+  } else if (inherits(y, "fh")) {
+    
+    if (any(is.na(y$model$std_real_residuals))) {
+      residuals <- y$model$std_real_residuals[!is.na(y$model$std_real_residuals)]
+      warning("At least one value in the standardized realized residuals is NA. Only
+              numerical values are plotted.")
+    } else {
+      residuals <- y$model$std_real_residuals
+    }
+    residuals <- (residuals - mean(residuals)) / sd(residuals)
+    rand.eff <- y$model$random_effects
+    srand.eff <- (rand.eff - mean(rand.eff)) / sd(rand.eff)
+    tmp <- srand.eff
+  }
+  ## QQ Plots
+  # Residuals
+  res <- qplot(sample = residuals) +
+    geom_abline(colour = color[1]) +
+    ggtitle("Error term") + ylab("Quantiles of pearson residuals") +
+    xlab("Theoretical quantiles") + gg_theme
+  
+  # Random effects
+  ran <- ggplot(data.frame(tmp) ,aes(sample = tmp)) +
+    stat_qq(distribution = qnorm,dparams = list(mean = mean(tmp),
+                                                sd = sd(tmp))) +
+    geom_abline(intercept = 0, slope = 1,na.rm = TRUE, col = color[1]) +
+    ggtitle("Random effect") + ylab("Quantiles of random effects") +
+    xlab("Theoretical quantiles") + gg_theme
+  
+  invisible(grid.arrange(arrangeGrob(res, ran ,ncol = 2)))
+  }
+
+
+
+
