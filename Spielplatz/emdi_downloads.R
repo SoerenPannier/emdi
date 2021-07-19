d30979e9905a5283c788908bbbaeea661615934a
SoerenPannier <soeren.pannier@gmail.com>
2021-07-19 22:43:01 +0200

    theme size and saving

--- Spielplatz/emdi_downloads.R
+++ Spielplatz/emdi_downloads.R
@@ -5,10 +5,8 @@ x <- cran_stats(c("emdi"))
 x$downloads <- cumsum(x$downloads)
 
 
-theme_set(theme_gray(base_size = 40))
+theme_set(theme_gray(base_size = 16))
 
 ggplot(x, aes(end, downloads)) +
   geom_line(size = 2, color="#4F728E")  + ylab("Cumulative downloads") +
   xlab("Year") 
-ggsave("../Thesis/Meine/Presentation/Figures/emdi_downloads.pdf", 
-       width = 10, height = 10)
