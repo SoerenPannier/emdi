8a145e5ed4b4ffe9aa46d2b3e951e1482a4ad352
akreutzmann <ann-kristin.kreutzmann@fu-berlin.de>
2022-07-21 18:59:13 +0200

    Feature/order (#35)
    * correct order for weights and mse
    
    * fix for domains with only one observation
    
    * unsignificant change
    
    * delete backtranformation tests
    
    * final order change
    
    * change scale point documentation
    
    Co-authored-by: FelixSkarke <felix.skarke@gmail.com>

--- R/mse_estimation.R
+++ R/mse_estimation.R
@@ -81,6 +81,8 @@ parametric_bootstrap <- function(framework,
   if (.Platform$OS.type == "windows") {
     flush.console()
   }
+
+
   mses <- apply(mses, c(1, 2), mean)
   mses <- data.frame(Domain = unique(framework$pop_domains_vec), mses)
 
@@ -192,6 +194,7 @@ mse_estim <- function(framework,
       vu_tmp = superpop$vu_tmp
     )
   }
+
   framework$smp_data <- bootstrap_sample
 
   # Prediction of indicators with bootstap sample.
