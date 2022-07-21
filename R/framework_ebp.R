c741f8543fe72d01c1fdbdd84227b1cb6311aa4e
Ann-KristinKreutzmann <annkristinkreutzmann@gmail.com>
2022-07-21 17:43:09 +0200

    final order change

--- R/framework_ebp.R
+++ R/framework_ebp.R
@@ -36,17 +36,20 @@ framework_ebp <- function(fixed, pop_data, pop_domains, smp_data, smp_domains,
                  function ebp."))
   }
 
+
   # Order of domains
   pop_data <- pop_data[order(pop_data[[pop_domains]]), ]
+  levels_tmp <- unique(pop_data[[pop_domains]])
   pop_data[[pop_domains]] <- factor(pop_data[[pop_domains]],
-    levels = unique(pop_data[[pop_domains]])
-  )
+                                    levels = levels_tmp)
   pop_domains_vec <- pop_data[[pop_domains]]
 
+
   smp_data[[smp_domains]] <- factor(smp_data[[smp_domains]],
-    levels = unique(pop_data[[pop_domains]])
-  )
+                                    levels = levels_tmp)
+  rm(levels_tmp)
   smp_data <- smp_data[order(smp_data[[smp_domains]]), ]
+
   smp_domains_vec <- smp_data[[smp_domains]]
   smp_domains_vec <- droplevels(smp_domains_vec)
 
@@ -74,6 +77,7 @@ framework_ebp <- function(fixed, pop_data, pop_domains, smp_data, smp_domains,
   # Number of households in sample per domain
   smp_domains_vec_tmp <- as.numeric(smp_domains_vec)
   n_smp <- as.vector(table(smp_domains_vec_tmp))
+
 
   # Indicator variables that indicate if domain is in- or out-of-sample
   obs_dom <- pop_domains_vec %in% unique(smp_domains_vec)
