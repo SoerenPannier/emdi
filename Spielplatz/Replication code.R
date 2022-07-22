0d6c3899fe7a73de06f1b17aa1c9f99a7e0f81ee
SPAN <soeren.pannier@ppi.de>
2022-07-22 10:18:58 +0200

    replication code updated

--- Spielplatz/Replication code.R
+++ Spielplatz/Replication code.R
@@ -1,16 +1,16 @@
 ######################################################################
-# Replication code for "A Framework for Producing Small Area Estimates 
+# Replication code for "A Framework for Producing Small Area Estimates
 # Based on Area-Levels in R"
 ######################################################################
 
 # Install the package
-if (!require("emdi")) {
-  install.packages("emdi")
-}
+
+install.packages("emdi",dependencies = TRUE)
+
 
 ####################
-# Code in Section 4.1 Estimation procedure for the standard 
-# Fay-Herriot model 
+# Code in Section 4.1 Estimation procedure for the standard
+# Fay-Herriot model
 ####################
 
 # Load package and population and sample data
@@ -19,9 +19,9 @@ data("eusilcA_popAgg")
 data("eusilcA_smpAgg")
 
 ## Combine input data
-combined_data <- combine_data(pop_data = eusilcA_popAgg, 
-                              pop_domains = "Domain", 
-                              smp_data = eusilcA_smpAgg, 
+combined_data <- combine_data(pop_data = eusilcA_popAgg,
+                              pop_domains = "Domain",
+                              smp_data = eusilcA_smpAgg,
                               smp_domains = "Domain")
 
 ## Identify spatial structures
@@ -34,70 +34,70 @@ rel <- spdep::poly2nb(austria_shape, row.names = austria_shape$PB)
 eusilcA_prox <- spdep::nb2mat(rel, style = "W", zero.policy = TRUE)
 
 # Spatial correlation tests
-spatialcor.tests(direct = combined_data$Mean, 
+spatialcor.tests(direct = combined_data$Mean,
                  corMatrix = eusilcA_prox)
 
 ## Perform model selection
 # Generate initial object of class "fh", "emdi"
-fh_std <- fh(fixed = Mean ~ cash + self_empl + unempl_ben, 
-             vardir = "Var_Mean", combined_data = combined_data, 
+fh_std <- fh(fixed = Mean ~ cash + self_empl + unempl_ben,
+             vardir = "Var_Mean", combined_data = combined_data,
              domains = "Domain", method = "ml", B = c(0,50))
 # Perform stepwise variable selection
 step(fh_std, criteria = "KICb2")
 
 ## Estimate EBLUPs and MSEs
 # Generate object of class "fh","emdi" with MSE estimation
-fh_std <- fh(fixed = Mean ~ cash + self_empl, vardir = "Var_Mean", 
-             combined_data = combined_data, domains = "Domain", 
+fh_std <- fh(fixed = Mean ~ cash + self_empl, vardir = "Var_Mean",
+             combined_data = combined_data, domains = "Domain",
              method = "ml", MSE = TRUE, B = c(0,50))
 
 ## Assess the estimated model
 summary(fh_std)
 plot(fh_std)
 
-## Compare results with direct estimates                                 
+## Compare results with direct estimates
 compare_plot(fh_std, CV = TRUE, label = "no_title")
 compare(fh_std)
 
 ## Benchmarking for consistent estimates
-# The benchmark value is calculated by taking the mean of the variable 
+# The benchmark value is calculated by taking the mean of the variable
 # eqIncome of the eusilcA_smp data frame of package emdi.
 data("eusilcA_smp")
 mean(eusilcA_smp$eqIncome)
-fh_bench <- benchmark(fh_std, benchmark = 20140.09, 
+fh_bench <- benchmark(fh_std, benchmark = 20140.09,
                       share = eusilcA_popAgg$ratio_n, type = "ratio")
 # Showing the first 6 rows of the results with head command
 head(fh_bench)
 
 ## Extract and visualize the results
-# EBLUP and MSE results (Showing the first 6 rows of the results with 
+# EBLUP and MSE results (Showing the first 6 rows of the results with
 # head command)
 head(estimators(fh_std, MSE = TRUE))
 
 # Visualization of results on maps
-# Load shape file 
+# Load shape file
 load_shapeaustria()
 
 # Maps with adjusted scales
-map_plot(object = fh_std, MSE = TRUE, 
+map_plot(object = fh_std, MSE = TRUE,
          map_obj = shape_austria_dis, map_dom_id = "PB",
-         scale_points = list(Direct = list(ind = c(8000, 60000), 
-                                           MSE = c(200000, 10000000)), 
+         scale_points = list(Direct = list(ind = c(8000, 60000),
+                                           MSE = c(200000, 10000000)),
                              FH = list(ind = c(8000, 60000),
                                        MSE = c(200000, 10000000))))
 
 ## Export the results
-# Export to excel (Note that under Windows RTools needs to be installed 
-# or rather it is important to have a zip application (see also Details 
+# Export to excel (Note that under Windows RTools needs to be installed
+# or rather it is important to have a zip application (see also Details
 # in help(write.excel))
 write.excel(fh_std, file = "fh_std_output.xlsx", MSE = TRUE, CV = TRUE)
 
 ####################
-# Code in Section 4.2 Estimation of the extended area-level models 
+# Code in Section 4.2 Estimation of the extended area-level models
 ####################
 
 ## FH model with transformation
-# Generate object of class "fh","emdi" with arcsin 
+# Generate object of class "fh","emdi" with arcsin
 # transformation, bc backtransformation and bootstrap MSE estimation
 fh_arcsin <- fh(fixed = MTMED ~ cash + age_ben + rent + house_allow,
                 vardir = "Var_MTMED", combined_data = combined_data,
@@ -106,18 +106,18 @@ fh_arcsin <- fh(fixed = MTMED ~ cash + age_ben + rent + house_allow,
                 MSE = TRUE, mse_type = "boot")
 
 ## Spatial FH model
-# Generate object of class "fh","emdi" with spatial 
+# Generate object of class "fh","emdi" with spatial
 # correlation and analytical MSE estimation
 fh_spatial <- fh(fixed = Mean ~ cash + self_empl, vardir = "Var_Mean",
                  combined_data = combined_data, domains = "Domain",
-                 correlation = "spatial", corMatrix = eusilcA_prox, 
+                 correlation = "spatial", corMatrix = eusilcA_prox,
                  MSE = TRUE)
 
 ## Robust FH model
-# Generate object of class "fh","emdi" with robust estimation 
+# Generate object of class "fh","emdi" with robust estimation
 # method and pseudolinear MSE estimation
-fh_robust <- fh(fixed = Mean ~ cash + self_empl, vardir = "Var_Mean", 
-                combined_data = combined_data, domains = "Domain", 
+fh_robust <- fh(fixed = Mean ~ cash + self_empl, vardir = "Var_Mean",
+                combined_data = combined_data, domains = "Domain",
                 method = "reblup", MSE = TRUE, mse_type = "pseudo")
 
 ## Measurement error (ME) model
@@ -133,9 +133,10 @@ for(i in 1:M){
     Ci_array[2,2,i] <- eusilcA_smpAgg$Var_Cash[i]
 }
 
-# Generate object of class "fh","emdi" with ME method and 
+# Generate object of class "fh","emdi" with ME method and
 # Jackknife MSE estimation
-fh_yl <- fh(fixed = Mean ~ Cash, vardir = "Var_Mean", 
-            combined_data = eusilcA_smpAgg, domains = "Domain", 
-            method = "me", Ci = Ci_array, MSE = TRUE, 
+fh_yl <- fh(fixed = Mean ~ Cash, vardir = "Var_Mean",
+            combined_data = eusilcA_smpAgg, domains = "Domain",
+            method = "me", Ci = Ci_array, MSE = TRUE,
             mse_type = "jackknife")
+
