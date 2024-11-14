8b16859732eeae117cf019f16e0ef9087a6d8911
RaelK <raelkyalo471@gmail.com>
2024-11-14 17:24:58 +0100

    Added description -> test examples not yet added

--- R/summary.emdi.R
+++ R/summary.emdi.R
@@ -2,14 +2,16 @@
 #'
 #' Additional information about the data and model in small area estimation
 #' methods and components of an emdi object are extracted. The generic function
-#' summary has methods for classes "direct", "ebp" and "fh" and the returned
-#' object is suitable for printing  with the \code{print}.
-#' @param object an object of type "direct", "ebp" or "fh", representing point
-#' and MSE estimates. Objects differ depending on the estimation method.
+#' summary has methods for classes "direct", "ebp", "ebp_tf", "fh", and "fh_tf"
+#' and the returned object is suitable for printing  with the \code{print}.
+#' @param object an object of type "direct", "ebp", "ebp_tf", "fh", or "fh_tf",
+#' representing point and MSE estimates. Objects differ depending on the
+#' estimation method.
 #' @param ... additional arguments that are not used in this method.
-#' @return an object of type "summary.direct", "summary.ebp" or "summary.fh"
-#' with information about the sample and population data, the usage of
-#' transformation, normality tests and information of the model fit.
+#' @return an object of type "summary.direct", "summary.ebp", "summary.ebp_tf",
+#' "summary.fh" or "summary.fh_tf" with information about the sample and
+#'  population data, the usage of transformation, normality tests and
+#'  information of the model fit.
 #' @references
 #' Lahiri, P. and Suntornchost, J. (2015), Variable selection for linear mixed
 #' models with applications in small area estimation, The Indian Journal of
@@ -21,7 +23,8 @@
 #' R2 from generalized linear mixed-effects models. Methods in Ecology and
 #' Evolution, 4(2), 133-142.
 #' @seealso \code{\link{emdiObject}}, \code{\link{direct}}, \code{\link{ebp}},
-#' \code{\link{ebp_tf}}, \code{\link{fh}}, \code{\link[MuMIn]{r.squaredGLMM}},
+#' \code{\link{ebp_tf}}, \code{\link{fh}}, \code{\link{fh_tf}},
+#' \code{\link[MuMIn]{r.squaredGLMM}},
 #' \code{\link[moments]{skewness}},
 #' \code{\link[moments]{kurtosis}}, \code{\link[stats]{shapiro.test}}
 #' @examples
