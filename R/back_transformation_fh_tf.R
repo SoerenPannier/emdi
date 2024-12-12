f043a8ca565b5f21dbec2db2fd920db12f061f2a
yeonjoolee10 <84901622+yeonjoolee10@users.noreply.github.com>
2024-12-12 10:47:38 +0100

    correction of bt error

--- R/back_transformation_fh_tf.R
+++ R/back_transformation_fh_tf.R
@@ -8,17 +8,22 @@ log_bt_fh_tf <- function(data, eblup, subdomains, varu, varv){
   bt_in_smp$bt_mu_ij <- exp(bt_in_smp[[eblup]] + 0.5 *  g1_in)
 
   bt_out_smp <- data[data$ObsSub == "no", ]
-  g1_out <- varu * (1 + (bt_out_smp$gamma_i/bt_out_smp$gamma_i0) *
-                      (1 - 2 * bt_out_smp$gamma_ij))
-  bt_out_smp$bt_mu_ij <- exp(bt_out_smp[[eblup]] + 0.5 * g1_out)
+  if(dim(bt_out_smp)[1] != 0){
+    g1_out <- varu * (1 + (bt_out_smp$gamma_i/bt_out_smp$gamma_i0) *
+                        (1 - 2 * bt_out_smp$gamma_ij))
+    bt_out_smp$bt_mu_ij <- exp(bt_out_smp[[eblup]] + 0.5 * g1_out)
 
-  # naive back transformation for the out of sample grids in out of sample domains
-  bt_out_smp$bt_mu_ij <- ifelse(bt_out_smp$gamma_ij == 0 &
-                                  bt_out_smp$gamma_i0 == 0,
-                                exp(bt_out_smp[[eblup]]), bt_out_smp$bt_mu_ij)
+    # naive back transformation for the out of sample grids in out of sample domains
+    bt_out_smp$bt_mu_ij <- ifelse(bt_out_smp$gamma_ij == 0 &
+                                    bt_out_smp$gamma_i0 == 0,
+                                  exp(bt_out_smp[[eblup]]), bt_out_smp$bt_mu_ij)
 
-  bt_eblup <- rbind(bt_in_smp[, c("bt_mu_ij", subdomains)],
-                    bt_out_smp[, c("bt_mu_ij", subdomains)])
+    bt_eblup <- rbind(bt_in_smp[, c("bt_mu_ij", subdomains)],
+                      bt_out_smp[, c("bt_mu_ij", subdomains)])
+  }else if(dim(bt_out_smp)[1] == 0){
+    bt_eblup <- rbind(bt_in_smp[, c("bt_mu_ij", subdomains)])
+  }
+
   return(bt_eblup)
 }
 
@@ -33,26 +38,32 @@ arcsin_bt_fh_tf <- function(data, eblup, subdomains, varu, varv) {
                                        sd = sqrt(g1_in[k]))$value
   }
   bt_out_smp <- data[data$ObsSub == "no", ]
-  for (k in 1:length(bt_out_smp$mu_ij)) {
-    g1_out <- varu * (1 + (bt_out_smp$gamma_i/bt_out_smp$gamma_i0) *
-                        (1 - 2 * bt_out_smp$gamma_ij))
-    #-------------------------------------------------------------------------------
-    # Naive back transformation for out of sample grids in out of sample domains
-    if(bt_out_smp$gamma_ij[k] == 0 & bt_out_smp$gamma_i0[k] == 0){
-      if(bt_out_smp$mu_ij[k] < 0) {bt_out_smp$bt_mu_ij[k] <- (sin(0))^2}
-      else if(bt_out_smp$mu_ij[k] > pi/2) {bt_out_smp$bt_mu_ij[k] <- (sin(pi/2))^2}
-      else if(bt_out_smp$mu_ij[k] < pi/2 & bt_out_smp$mu_ij[k] > 0) {bt_out_smp$bt_mu_ij[k] <- (sin(bt_out_smp$mu_ij[k]))^2}
-    } else {
-      # bias corrected back transformation for out of sample grids but in sample domains
-      bt_out_smp$bt_mu_ij[k] <- integrate(integrand,
-                                          lower = 0,
-                                          upper = pi / 2,
-                                          bt_out_smp$mu_ij[k],
-                                          sqrt(g1_out[k]))$value
+  if(dim(bt_out_smp)[1] != 0){
+    for (k in 1:length(bt_out_smp$mu_ij)) {
+      g1_out <- varu * (1 + (bt_out_smp$gamma_i/bt_out_smp$gamma_i0) *
+                          (1 - 2 * bt_out_smp$gamma_ij))
+      #-------------------------------------------------------------------------------
+      # Naive back transformation for out of sample grids in out of sample domains
+      if(bt_out_smp$gamma_ij[k] == 0 & bt_out_smp$gamma_i0[k] == 0){
+        if(bt_out_smp$mu_ij[k] < 0) {bt_out_smp$bt_mu_ij[k] <- (sin(0))^2}
+        else if(bt_out_smp$mu_ij[k] > pi/2) {bt_out_smp$bt_mu_ij[k] <- (sin(pi/2))^2}
+        else if(bt_out_smp$mu_ij[k] < pi/2 & bt_out_smp$mu_ij[k] > 0) {bt_out_smp$bt_mu_ij[k] <- (sin(bt_out_smp$mu_ij[k]))^2}
+      } else {
+        # bias corrected back transformation for out of sample grids but in sample domains
+        bt_out_smp$bt_mu_ij[k] <- integrate(integrand,
+                                            lower = 0,
+                                            upper = pi / 2,
+                                            bt_out_smp$mu_ij[k],
+                                            sqrt(g1_out[k]))$value
+      }
+      #-------------------------------------------------------------------------------
     }
-    #-------------------------------------------------------------------------------
+    bt_eblup <- rbind(bt_in_smp[, c("bt_mu_ij", subdomains)],
+                      bt_out_smp[, c("bt_mu_ij", subdomains)])
+  } else if(dim(bt_out_smp)[1] == 0){
+    bt_eblup <- rbind(bt_in_smp[, c("bt_mu_ij", subdomains)])
   }
-  bt_eblup <- rbind(bt_in_smp[, c("bt_mu_ij", subdomains)],
-                    bt_out_smp[, c("bt_mu_ij", subdomains)])
+
+
   return(bt_eblup)
 }
