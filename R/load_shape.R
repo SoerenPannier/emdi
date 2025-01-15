b0178e6245756a8d0f10cca3c702cb3bdb4c46bc
yeonjoolee10 <84901622+yeonjoolee10@users.noreply.github.com>
2025-01-15 16:03:30 +0100

    add shape file at municipal level

--- R/load_shape.R
+++ R/load_shape.R
@@ -14,3 +14,20 @@ load_shapeaustria <- function() {
        envir = .GlobalEnv
   )
 }
+
+#' Loading the Shape File for Austrian Municipalities
+#'
+#' The function simplifies to load the shape file for Austrian municipalities.
+#'
+#' @return A shape file of class \code{"sf", "data.frame"}.
+#' @details The shape file contains the borders of Austrian municipalities.
+#' Thus, it can be used for the visualization of estimation results for Austrian
+#' municipalities.
+#' @export
+#'
+load_shapeaustria_mun <- function() {
+  load(file  = system.file("shapes/shape_austria_mun.rda",
+                           package = "emdi"),
+       envir = .GlobalEnv
+  )
+}
